9d8231ba1c8844b42fbdc768065131d8813550ae
Mitch <70587519+mitchellschul@users.noreply.github.com>
2022-12-22 00:18:37 -0500

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from 'react-router-dom';
+
+import Home from './components/Home.jsx';
+import Contact from './components/Contact.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route exact path='/' element={< Home />}></Route>
+          <Route exact path='/contact' element={< Contact />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
